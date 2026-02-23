ff7454cd90cb45e0aea7da581cd2e9ccea595e61
JunkyMoldyStore <rodrigoabelleira@gmail.com>
2026-02-23 13:45:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 mercadopago.configure({
-  access_token: "TU_ACCESS_TOKEN"
+  access_token: "TEST-APP_USR-4932167697942108-022310-69a008ad0781871f831eec848b64fd87-3222530842"
 });
 
 app.post("/crear-preferencia", async (req, res) => {
