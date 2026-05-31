e108d73f2b718972b9a820191dde7c23a6e14952
JunkyMoldyStore <rodrigoabelleira@gmail.com>
2026-05-31 03:14:42 -0300

    Update server.js

--- server.js
+++ server.js
@@ -112,28 +112,6 @@ app.post("/webhook", async (req, res) => {
   console.log("📩 WEBHOOK RECIBIDO");
   console.log(JSON.stringify(req.body, null, 2));
 
-  try {
-
-    if (req.body.type === "payment") {
-
-      const paymentId = req.body.data.id;
-
-      console.log("💰 ID DE PAGO:", paymentId);
-
-      const resultado = await mercadopago.payment.findById(paymentId);
-
-      console.log("📦 PAYMENT:");
-      console.log(JSON.stringify(resultado.body, null, 2));
-
-    }
-
-  } catch (error) {
-
-    console.error("❌ ERROR CONSULTANDO PAGO:");
-    console.error(error.message);
-
-  }
-
   res.sendStatus(200);
 
 });
